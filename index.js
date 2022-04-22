2c69f9e0c4e3413845594fad2410f667d08dae0e
Natalya Colburn <greatqueenbee@gmail.com>
2022-04-22 09:37:46 -0600

    wrote function to generate node read me instead of html

--- index.js
+++ index.js
@@ -10,9 +10,25 @@ const uswrPrompts = () => {
     return inquirer.prompt([
       {
         type: 'input',
-        name: 'name',
-        message: 'What is your Project name?',
+        name: 'title',
+        message: 'What is your Project title?',
       },
+      {
+        type: "input",
+        name: "author",
+        message: "What is the author's name?"
+    },
+
+      {
+        type: "input",
+        name: "username",
+        message: "What is your GitHub username?"
+    },
+    {
+        type: "input",
+        name: "email",
+        message: "What is your email address?"
+    },
       {
         type: 'list',
         name: 'license',
@@ -70,20 +86,42 @@ if(data.license == "MIT License"){
     badge = "![License](https://img.shields.io/static/v1?label=License&message=Eclipse.0&color=orange&style=plastic)"
 }else if (data.license == "Mozilla Public License 2.0"){
     badge = "![License](https://img.shields.io/static/v1?label=License&message=Mozilla.0&color=blue&style=plastic)"
+}    
 
 
  
   
 //   function generateMarkdown(data) {
 
-
-//     return `# ${data.title}
-//     ## license 
-//     ${renderLicenseBadge(license)}` 
+    return`# ${data.title}  
+    ${badge}
+    ${data.description}
+    ## Table of Contents:
+    * [Installation](#install)
+    * [Usage](#usage)
+    * [License](#license)
+    * [Contributing](#contribute)
+    * [Tests](#tests)
+    * [Questions](#questions)
+    ### Installation:
+    In order to install the necessary dependencies, open the console and run the following:
+    \`\`\`${data.install}\`\`\`
+    ### Usage:
+    ${data.usage}
+    ### License:
+    This project is licensed under:
+    ${data.license}
+    ### Contributing:
+    ${data.contribute}
+    ### Tests:
+    In order to test open the console and run the following:
+    \`\`\`${data.tests}\`\`\`
+    ### Questions:
+    If you have any questions contact me on [GitHub](https://github.com/${data.username}) or contact 
+    ${data.author} at ${data.email}  
+     `
+    }
     
-    
-//     }
-// console.log(data);
 
 uswrPrompts()
 .then((data) => writeFileAsync('generatedREADME.md', generateMD(data)))
